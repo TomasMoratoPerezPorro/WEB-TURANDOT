d238039dd2f7b58b1661fee75db0a43cc1fff47e
TomasMoratoPerezPorro <49519581+TomasMoratoPerezPorro@users.noreply.github.com>
2020-02-27 18:23:31 +0100

    Carousel aded

---
+++ assets/carta.js
@@ -0,0 +1,129 @@
+var picar = [
+        {
+            titol: "COCA DEL 'MOSSÈN DE FOLGUEROLES'"
+        },
+        {
+            titol: "OLIVES VARIADES"
+        },
+        {
+            titol: "COL KALE FREGIDA AMB SÈSAM DAURAT "
+        },
+        {
+            titol: "ANXOVES DEL CANTÀBRIC "
+        },
+        {
+            titol: "GAMBES DE PALAMÓS"
+        },
+        {
+            titol: "CROQUETES DE: XIPIRÓ / CEPS I TÒFONA / POLLASTRE ROSTIT"
+        },
+        {
+            titol: "ESPATLLA IBÈRICA D’AGLÀ, 'SEÑORÍO DE MONTANERA D.O. EXTREMADURA'"
+        },
+        {
+            titol: "ASSORTIMENT DE FORMATGES CATALANS ARTESANALS"
+        }
+    ];
+
+var primers = [
+        {
+            titol: "AMANIDA VERDA DE TARDOR,",
+            descripcio: "MESCLUM, FRUITS SECS, MOSTASSA I MEL"
+        },
+        {
+            titol: "VERAT ADOBAT A CASA FET A LA FLAMA,",
+            descripcio: "ALBERGÍNIA ESCALIVADA, OLIVES NEGRES"
+        },
+        {
+            titol: "ESPAGUETIS DE CELERÍ,",
+            descripcio: "CARBONARA D’ANGUILA FUMADA"
+        },
+        {
+            titol: "PICANHA MADURADA CURADA EN SAL D´HERBES,",
+            descripcio: "CEPS SALTATS, TÀPERES, PARMESÀ I BROTS"
+        },
+        {
+            titol: "CREMA DE CARBASSA,",
+            descripcio: "PERFUMS ASIÀTICS, OLI DE SÀLVIA"
+        },
+        {
+            titol: "OU 62º",
+            descripcio: "ESCUMA DE PATATA TRUFADA I ROSSINYOLS SALTATS"
+        },
+        {
+            titol: "CANELONS,",
+            descripcio: "POLLASTRE ROSTIT AMB SALSA DE FOIE DE “CAN MUNNAR”"
+        },
+        {
+            titol: "SALTAT DE BOLETS SILVESTRES,",
+            descripcio: "ALL, JULIVERT I BUTIFARRA NEGRA DE 'CAN RALIU'"
+        },
+        {
+            titol: "CALAMAR DE POTERA,",
+            descripcio: "FARCIT DE VERDURETES I FETGE DE RAP"
+        },
+        {
+            titol: "VIEIRES,",
+            descripcio: "CARXOFES EN TEXTURES I PERNIL D’ÀNEC"
+        }
+    ];
+
+    var segons = [
+        {
+            titol: "LLUÇ DE PALANGRE AL FORN,",
+            descripcio: "PATATES A L’ESTIL DE L’EMPORDÀ"
+        },
+        {
+            titol: "LLOBARRET A LA PLANXA,",
+            descripcio: "PATATA XAFADA I CREMA DE GÍRGOLES I XAMPINYONS"
+        },
+        {
+            titol: "“FISH & XIPS” DE BACALLÀ,",
+            descripcio: "EL SEU PIL-PIL, LLARDONS I CEBETES ENCURTIDES"
+        },
+        {
+            titol: "ARRÒS A LA CASSOLA,",
+            descripcio: "SALSITXES I BOLETS SILVESTRES"
+        },
+        {
+            titol: "MAGRET D’ÀNEC DE COLOMERS,",
+            descripcio: "POMA I FOIE DE 'CAN MUNNAR'"
+        },
+        {
+            titol: "COLL DE XAI SENSE FEINA,",
+            descripcio: "VERDURETES ROSTIDES I EL SEU SUC"
+        },
+        {
+            titol: "TURBOT EN SUQUET,",
+            descripcio: "PATATA I CLOÏSSES"
+        },
+        {
+            titol: " TERRINA DE GARRINET DE SEGÒVIA",
+            descripcio: "MOLE MEXICÀ I ORELLANES "
+        },
+        {
+            titol: "ENTRECOT DE VACA MADURADA",
+            descripcio: "PATATES FREGIDES I PEBROTS DEL PADRÓ"
+        }
+    ];
+     
+    var postres= [
+        {
+            titol: "EL NOSTRE CHEESECAKE"
+        },
+        {
+            titol: "COULANT DE XOCOLATA"
+        },
+        {
+            titol: "BABÀ AL RON CREMAT"
+        },
+        {
+            titol: "TIRAMISÚ DE RECUIT"
+        },
+        {
+            titol: "UNA MENA DE LEMON PIE"
+        },
+        {
+            titol: "ASSORTIMENT DE FORMATGES CATALANS"
+        }
+    ];
---
+++ assets/js/scripts.js
@@ -0,0 +1,169 @@
+"use strict";
+const buttons = document.querySelectorAll('.button');
+
+var Navbar = function () {
+	function dark() {
+		$(".navbar").removeClass("navbar-light").addClass("navbar-dark")
+	}
+	function light() {
+		$(".navbar").removeClass("navbar-dark").addClass("navbar-light")
+	}
+
+	function scrollMenu() {
+		5 < $(window).scrollTop() ? dark() : light()
+	}
+	scrollMenu(), $(window).on({
+		scroll: function () {
+			scrollMenu()
+		},
+	})
+}(),
+
+	Dishes = function () {
+		var e = $(".carousel");
+		e.length && e.each(function () {
+			$(this).flickity({
+				cellAlign: 'left',
+				contain: true
+			})
+		})
+	}()
+
+
+$(document).ready(function () {
+	$(".mybutton").click(function () {
+		//alert("The paragraph was clicked.");
+		buttons.forEach(button => button.classList.remove('active'));
+		this.classList.add('active');
+		if ($(this).attr("id") == "picapica") {
+			console.log("this is picapica");
+			listaPicaPica();
+		} else if ($(this).attr("id") == "primers") {
+			console.log("this is primers");
+			listaPrimers();
+		} else if ($(this).attr("id") == "segons") {
+			console.log("this is segons");
+			listaSegons();
+		} else {
+			console.log("this is postre");
+			listaPostres();
+		}
+	});
+});
+
+function listaPicaPica() {
+	$.ajax({
+		url: 'assets/carta.json',
+		type: 'get',
+		datatype: 'json',
+		beforeSend: function () {
+			$("#contenidos").html("Procesando, espere por favor...");
+		},
+		success: function (data) {
+			//var resposta=JSON.parse(data);
+			console.log(data);
+			var resposta = data;
+			console.log(resposta);
+			$("#contenidos").empty();
+			$.each(resposta.picapica, function (index, element) {
+				$("#contenidos").append('<div class="item"><div class="item__header"><h3 class="item__title">' + element.titol + '<h3><span class="item__dots"></span><span class="item__price"></span></div></div>');
+				//$("#contenidos").append('<p>'+element.titol+'</p>');
+			});
+		},
+		error: function (e) {
+			//called when there is an error
+			console.log(e.message);
+			$("#contenidos").html("Ha habido un error " + e.message);
+		}
+
+	});
+}
+
+function listaPrimers() {
+	$.ajax({
+		url: 'assets/carta.json',
+		type: 'get',
+		datatype: 'json',
+		beforeSend: function () {
+			$("#contenidos").html("Procesando, espere por favor...");
+		},
+		success: function (data) {
+			//var resposta=JSON.parse(data);
+			console.log(data);
+			var resposta = data;
+			console.log(resposta);
+			$("#contenidos").empty();
+			$.each(resposta.primers, function (index, element) {
+				$("#contenidos").append('<div class="item"><div class="item__header"><h3 class="item__title">' + element.titol + '<h3><span class="item__dots"></span><span class="item__price"></span></div><p class="item__description">' + element.descripcio + '</p></div>');
+				//$("#contenidos").append('<p>'+element.titol+'</p>');
+			});
+		},
+		error: function (e) {
+			//called when there is an error
+			console.log(e.message);
+			$("#contenidos").html("Ha habido un error " + e.message);
+		}
+
+	});
+}
+
+function listaSegons() {
+	$.ajax({
+		url: 'assets/carta.json',
+		type: 'get',
+		datatype: 'json',
+		beforeSend: function () {
+			$("#contenidos").html("Procesando, espere por favor...");
+		},
+		success: function (data) {
+			//var resposta=JSON.parse(data);
+			console.log(data);
+			var resposta = data;
+			console.log(resposta);
+			$("#contenidos").empty();
+			$.each(resposta.segons, function (index, element) {
+				$("#contenidos").append('<div class="item"><div class="item__header"><h3 class="item__title">' + element.titol + '<h3><span class="item__dots"></span><span class="item__price"></span></div><p class="item__description">' + element.descripcio + '</p></div>');
+				//$("#contenidos").append('<p>'+element.titol+'</p>');
+			});
+		},
+		error: function (e) {
+			//called when there is an error
+			console.log(e.message);
+			$("#contenidos").html("Ha habido un error " + e.message);
+		}
+
+	});
+}
+
+function listaPostres() {
+	$.ajax({
+		url: 'assets/carta.json',
+		type: 'get',
+		datatype: 'json',
+		beforeSend: function () {
+			$("#contenidos").html("Procesando, espere por favor...");
+		},
+		success: function (data) {
+			//var resposta=JSON.parse(data);
+			console.log(data);
+			var resposta = data;
+			console.log(resposta);
+			$("#contenidos").empty();
+			$.each(resposta.postres, function (index, element) {
+				$("#contenidos").append('<div class="item"><div class="item__header"><h3 class="item__title">' + element.titol + '<h3><span class="item__dots"></span><span class="item__price"></span></div></div>');
+				//$("#contenidos").append('<p>'+element.titol+'</p>');
+			});
+		},
+		error: function (e) {
+			//called when there is an error
+			console.log(e.message);
+			$("#contenidos").html("Ha habido un error " + e.message);
+		}
+
+	});
+}
+
+
+
+
+
